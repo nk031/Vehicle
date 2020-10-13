765704a9fc9adb5826983525fc4121a626bf3e83
Navjot Kaur <NK031@atcltd.atc.org.nz>
2020-10-13 22:03:42 +1300

    Add project files.

---
+++ Vehicle/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Vehicle
+{
+    partial class Vehicle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartBt = new System.Windows.Forms.Button();
+            this.Bicycle = new System.Windows.Forms.PictureBox();
+            this.Plane = new System.Windows.Forms.PictureBox();
+            this.Toyata = new System.Windows.Forms.PictureBox();
+            this.Bmw = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Bicycle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plane)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Toyata)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bmw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartBt
+            // 
+            this.StartBt.Location = new System.Drawing.Point(241, 230);
+            this.StartBt.Name = "StartBt";
+            this.StartBt.Size = new System.Drawing.Size(74, 48);
+            this.StartBt.TabIndex = 4;
+            this.StartBt.Text = "StartHere";
+            this.StartBt.UseVisualStyleBackColor = true;
+            this.StartBt.Click += new System.EventHandler(this.StartBt_Click);
+            // 
+            // Bicycle
+            // 
+            this.Bicycle.Image = global::Vehicle.Properties.Resources.Bicycle;
+            this.Bicycle.Location = new System.Drawing.Point(9, 366);
+            this.Bicycle.Name = "Bicycle";
+            this.Bicycle.Size = new System.Drawing.Size(127, 75);
+            this.Bicycle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Bicycle.TabIndex = 3;
+            this.Bicycle.TabStop = false;
+            // 
+            // Plane
+            // 
+            this.Plane.Image = global::Vehicle.Properties.Resources.Plane;
+            this.Plane.Location = new System.Drawing.Point(11, 255);
+            this.Plane.Name = "Plane";
+            this.Plane.Size = new System.Drawing.Size(126, 93);
+            this.Plane.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Plane.TabIndex = 2;
+            this.Plane.TabStop = false;
+            // 
+            // Toyata
+            // 
+            this.Toyata.Image = global::Vehicle.Properties.Resources.Toyato;
+            this.Toyata.Location = new System.Drawing.Point(10, 132);
+            this.Toyata.Name = "Toyata";
+            this.Toyata.Size = new System.Drawing.Size(127, 105);
+            this.Toyata.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Toyata.TabIndex = 1;
+            this.Toyata.TabStop = false;
+            // 
+            // Bmw
+            // 
+            this.Bmw.Image = global::Vehicle.Properties.Resources.Bmw;
+            this.Bmw.Location = new System.Drawing.Point(9, 9);
+            this.Bmw.Name = "Bmw";
+            this.Bmw.Size = new System.Drawing.Size(129, 109);
+            this.Bmw.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Bmw.TabIndex = 0;
+            this.Bmw.TabStop = false;
+            this.Bmw.Click += new System.EventHandler(this.Bmw_Click);
+            // 
+            // Vehicle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StartBt);
+            this.Controls.Add(this.Bicycle);
+            this.Controls.Add(this.Plane);
+            this.Controls.Add(this.Toyata);
+            this.Controls.Add(this.Bmw);
+            this.Name = "Vehicle";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.Bicycle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plane)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Toyata)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bmw)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Bmw;
+        private System.Windows.Forms.PictureBox Toyata;
+        private System.Windows.Forms.PictureBox Plane;
+        private System.Windows.Forms.PictureBox Bicycle;
+        private System.Windows.Forms.Button StartBt;
+    }
+}
+
---
+++ Vehicle/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vehicle
+{
+    public partial class Vehicle : Form
+
+    {
+        Random rand = new Random();
+        CarRace Class = new CarRace();
+        public Vehicle()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBt_Click(object sender, EventArgs e)
+        {
+            Move();
+        }
+        
+          public new async void Move()
+
+        {
+            
+            {
+                Bmw.Location = new Point(Bmw.Location.X + 10, Bmw.Location.Y);
+                await Task.Delay(10);
+                Toyata.Location = new Point(Toyata.Location.X + 25, Toyata.Location.Y);
+                await Task.Delay(25);
+                Plane.Location = new Point(Plane.Location.X + 30, Plane.Location.Y);
+                await Task.Delay(30);
+                Bicycle.Location = new Point(Bicycle.Location.X + 15, Bicycle.Location.Y);
+                await Task.Delay(15);
+
+            }
+            while (Bmw.Location.X < 500)
+            {
+                int speed1 = rand.Next(0, 20);
+                Bmw.Location = new Point(Bmw.Location.X + speed1, Bmw.Location.Y);
+                await Task.Delay(25);
+                int speed2 = rand.Next(0, 20);
+                Toyata.Location = new Point(Toyata.Location.X + speed2, Toyata.Location.Y);
+                await Task.Delay(25);
+                int speed3 = rand.Next(0, 20);
+                Plane.Location = new Point(Plane.Location.X + speed3, Plane.Location.Y);
+                await Task.Delay(30);
+                int speed4 = rand.Next(0, 20);
+                Bicycle.Location = new Point(Bicycle.Location.X + speed4, Bicycle.Location.Y);
+                await Task.Delay(15);
+            }
+
+        }
+    
+
+        private void Bmw_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
+
+
+    
+
